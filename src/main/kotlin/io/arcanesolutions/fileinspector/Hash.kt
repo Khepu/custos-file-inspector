4632d13d52af39cccfe3566e3b19d0525fd8f1ca
Khepu <makris.giwrgos1234@gmail.com>
2022-02-14 01:51:01 +0200

    Feature: Add utility hash functions

---
+++ src/main/kotlin/io/arcanesolutions/fileinspector/Hash.kt
@@ -0,0 +1,23 @@
+package io.arcanesolutions.fileinspector
+
+import java.nio.charset.StandardCharsets.UTF_8
+import java.security.MessageDigest
+import java.util.Base64
+
+fun hash(bytes: ByteArray, hashFunction: String) =
+    Base64
+        .getEncoder()
+        .encode(
+            MessageDigest
+                .getInstance(hashFunction)
+                .digest(bytes))
+        .toString(UTF_8)
+
+fun md5(bytes: ByteArray) =
+    hash(bytes, "MD5")
+
+fun sha256(bytes: ByteArray) =
+    hash(bytes, "SHA256")
+
+fun sha1(bytes: ByteArray) =
+    hash(bytes, "SHA1")
