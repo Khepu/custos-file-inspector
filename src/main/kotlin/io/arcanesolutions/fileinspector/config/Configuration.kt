89dff496897d6414fc199734f39822fc6e40e759
Khepu <makris.giwrgos1234@gmail.com>
2022-02-20 01:11:50 +0200

    Feature: Configurable maximum file size

--- src/main/kotlin/io/arcanesolutions/fileinspector/config/Configuration.kt
+++ src/main/kotlin/io/arcanesolutions/fileinspector/config/Configuration.kt
@@ -2,6 +2,10 @@ package io.arcanesolutions.fileinspector.config
 
 
 data class Configuration(
+    /**
+     * Maximum accepted file size in bytes.
+     */
+    val maxFileSize: Int,
     val rabbitConnection: RabbitConnection,
     val inspectionRequest: RabbitInput,
     val inspectionResponse: RabbitOutput,
